1a98856e06565941c92b7ea92261d6a56509a4c4
csdoker <csd758371536@qq.com>
2019-07-28 14:55:29 +0800

    修改wepback配置，打包文件到docs目录，供github pages使用

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'), // dist
     filename: 'gobang.min.js'
   },
   plugins: [
